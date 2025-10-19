01acfa4f006288f6d049f2109a0d7d3603681840
barackwilliam <barackwilliam12@gmail.com>
2025-10-19 17:17:47 +0300

    trial

--- dpps_app/urls.py
+++ dpps_app/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
 
 
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     
     # Dashboard and profile URLs
--- dpps_app/views.py
+++ dpps_app/views.py
@@ -233,7 +233,7 @@ def register(request):
                 participation_date=date.today(),
                 status='pending'
             )
-            messages.success(request, 'Registration successful! Please wait for admin approval.')
+            messages.success(request, 'Registration successful! Fill form below to login.')
             return redirect('login')
     else:
         form = UserRegistrationForm()
@@ -310,3 +310,24 @@ from django.contrib.auth import logout
 def logout_view(request):
     logout(request)
     return redirect('login')  # or wherever you want after logout
+
+
+
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')  # Hapa ni sehemu ambapo unataka mtumiaji aende baada ya ku-login.
+        else:
+            # Hapa tunawonyesha error message
+            messages.error(request, "Invalid username or password")
+            return render(request, 'login.html')  # Re-render the login page
+    return render(request, 'login.html')  # Initial rendering
